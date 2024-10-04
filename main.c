9e5f6f68c52855f5efd66be22e1e5335c8373ec2
lmmas <louismmassin@gmail.com>
2024-10-04 20:52:32 +0200

    commit commit

--- main.c
+++ main.c
@@ -1,21 +1,18 @@
 #include "main.h"
 #include "q3.h"
-#include "q2.h"
+#include "projection.h"
 
 int main(void) {
     //mainMemoryTest();
     //memoryTest();
 
-    display_question3();
+    //display_question3();
 
-    getSizeFile();
     return 0;
 }
 
 
-
-/*
 void mainMemoryTest() {
     printf("address of the main function: %p \n", main);
 
-}*/
+}
