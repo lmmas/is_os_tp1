84da3b8f90a8a65f723ab004877f89a7ec016a3e
CassandreChandelier <cassandre.chandelier@ensea.fr>
2024-10-04 23:00:32 +0200

    question 2

---
+++ cmake-build-debug/CMakeFiles/is_os_tp1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/is_os_tp1.dir/main.c.o"
+  "CMakeFiles/is_os_tp1.dir/main.c.o.d"
+  "CMakeFiles/is_os_tp1.dir/memory.c.o"
+  "CMakeFiles/is_os_tp1.dir/memory.c.o.d"
+  "CMakeFiles/is_os_tp1.dir/q2.c.o"
+  "CMakeFiles/is_os_tp1.dir/q2.c.o.d"
+  "CMakeFiles/is_os_tp1.dir/q3.c.o"
+  "CMakeFiles/is_os_tp1.dir/q3.c.o.d"
+  "is_os_tp1"
+  "is_os_tp1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/is_os_tp1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/is_os_tp1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for is_os_tp1.
+# This may be replaced when dependencies are built.
